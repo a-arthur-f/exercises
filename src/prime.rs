4d9e462dc6ea9107d1fa599081179e3834dfef6e
Arthur Fusco <arthur.fusco@mrcat.com.br>
2023-08-04 18:21:02 -0300

    feat: created prime module

---
+++ src/prime.rs
@@ -0,0 +1,36 @@
+use rand::{thread_rng, Rng};
+
+pub fn is_prime(number: u128, rounds: usize) -> bool {
+    if number == 2 || number == 3 {
+        return true;
+    }
+
+    if number % 2 == 0 {
+        return false;
+    }
+
+    let mut rng = thread_rng();
+    let mut s = 0;
+    let mut d = (number - 1) as u32;
+
+
+    while d & 1 == 0 {
+        d >>= 1;
+        s += 1;
+    }
+
+    for _ in 0..rounds {
+        let a = rng.gen_range(2..=number - 2);
+        let mut x = a.pow(d) % number;
+
+        if x == 1 || x == number - 1 { return true; }
+
+        for _ in 0..s {
+            x = x.pow(2) % number;
+
+            if x == number - 1 { return true; }
+        }
+    }
+
+    false
+}
