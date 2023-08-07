c880c5b5699e787d34d9e4d61cf8cf8a638ea706
Arthur Fusco <arthur.fusco@mrcat.com.br>
2023-08-07 15:22:51 -0300

    feat: created average mod

---
+++ src/average.rs
@@ -0,0 +1,29 @@
+pub struct Student {
+    grades: Vec<Grade>
+}
+
+pub struct Grade(pub f32);
+
+impl Student {
+    pub fn new() -> Student {
+        Student {
+            grades: vec![]
+        }
+    }
+
+    pub fn new_grade(&mut self, grade: Grade) {
+        self.grades.push(grade);
+    }
+
+    pub fn get_average(&self) -> f32 {
+        let mut total: f32 = 0f32;
+
+        for grade in self.grades.iter() {
+            match grade {
+                Grade(value) => total += value
+            }
+        }
+
+        total / self.grades.len() as f32
+    }
+}
