966b0b7ce286227433f4bc0467a9a3aafd0b9e10
Arthur Fusco <arthur.fusco@mrcat.com.br>
2023-08-08 12:32:15 -0300

    style: convert identifier to snake case

--- src/vowel.rs
+++ src/vowel.rs
@@ -6,7 +6,7 @@ pub fn count(string: &str) -> (u32, HashMap<char, u32>) {
         HashMap::from([('a', 0), ('e', 0), ('i', 0), ('o', 0), ('u', 0)]);
 
     for ch in string.to_ascii_lowercase().chars() {
-        if isVowel(ch) {
+        if is_vowel(ch) {
             total += 1;
             let old_char_counting = vowels_counting.get(&ch).unwrap();
             vowels_counting.insert(ch, old_char_counting + 1);
@@ -16,7 +16,7 @@ pub fn count(string: &str) -> (u32, HashMap<char, u32>) {
     (total, vowels_counting)
 }
 
-fn isVowel(ch: char) -> bool {
+fn is_vowel(ch: char) -> bool {
     match ch.to_ascii_lowercase() {
         'a' | 'e' | 'i' | 'o' | 'u' => true,
         _ => false,
