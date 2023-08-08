ab629338427b11a9596c813b5dc754a04b0488f7
Arthur Fusco <arthur.fusco@mrcat.com.br>
2023-08-08 10:19:55 -0300

    feat: rewrite is_palindrome()

--- src/palindrome.rs
+++ src/palindrome.rs
@@ -1,7 +1,16 @@
 pub fn is_palindrome(s: &str) -> bool {
-    let half = s.len() / 2;
+    let chars: Vec<char> = s.chars().collect();
+    let mut low: usize = 0;
+    let mut high = chars.len() - 1;
 
-    s.to_lowercase()[..=half]
-        .chars()
-        .eq(s.to_lowercase()[half..].chars().rev())
+    while high > low {
+        if chars[low] != chars[high] {
+            return false;
+        }
+
+        low += 1;
+        high -= 1;
+    }
+
+    true
 }
